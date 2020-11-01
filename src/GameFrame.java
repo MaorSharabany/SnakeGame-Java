b62029fc11b8dbb0d20e928c2825e1e16cab0a4e
Maor Sharabany <maorshaharabani@gmail.com>
2020-11-01 11:08:07 +0200

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,19 +2,13 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 	
-	GameFrame(){
-		/*
-		GamePanel panel = new GamePanel();
-		this.add(panel);*/
-		
+	GameFrame(){	
 		this.add(new GamePanel());
 		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
 		this.setVisible(true);
-		this.setLocationRelativeTo(null);
-		
-	}
-	
+		this.setLocationRelativeTo(null);		
+	}	
 }
